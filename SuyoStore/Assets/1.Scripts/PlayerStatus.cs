38b46bf0dec64e91c63f68c59a58fa89ab96d829
ise <ydkrkek@gmail.com>
2022-03-29 23:55:35 +0900

    status 스크립트 기반으로 playerStatuts의 HP 작어

--- SuyoStore/Assets/1.Scripts/PlayerStatus.cs
+++ SuyoStore/Assets/1.Scripts/PlayerStatus.cs
@@ -1,26 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStatus
+public class PlayerStatus : MonoBehaviour
 {
-    public int hp; // 체력
-    public int satiety; // 포만감
-    public int fatigue; // 피로도
-    public int attack; // 공격력
-    public int stamina; // 지구력
-
     public int maxHp = 100;
     public int maxSatiety = 100;
     public int maxFatique = 100;
 
-    private float speed; // 스피드
+    public int currentHp; // 체력
+    public int currentSatiety; // 포만감
+    public int currentFatigue; // 피로도
+    public int currentAttack; // 공격력
+    public int currentStamina; // 지구력
 
-    public void SetSpeed(float speed)
+    void Awake()
     {
-        if (speed > 0) this.speed = speed;
+        currentHp = 10;
     }
-    public float GetSpeed() {
-        return speed;
+
+
+    public void TakeDamage(int damage)
+    {
+        currentHp -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damage.");
+    
+        // 게임 오버 기준
+        if(currentHp <= 0)
+        {
+            Die();
+        }
+
+        // 회복
+            // 치료제 아이템 사용
+            // 가구, 침낭 아이템 사용
     }
+
+    public virtual void Die()
+    {
+
+    }
+    //private float speed; // 스피드
+
+    //public void SetSpeed(float speed)
+    //{
+    //    if (speed > 0) this.speed = speed;
+    //}
+    //public float GetSpeed() {
+    //    return speed;
+    //}
 }
