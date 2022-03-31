14c67c23b22112de8683d060bcbd9cff7aebb1de
Ji Hae Kim (ji-kim) <53713342+deftones88@users.noreply.github.com>
2022-03-31 16:09:31 +0900

    [UPDATE] database

---
+++ SuyoStore/Assets/1.Scripts/Item/Editor/CustomEditorWindow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CustomEditorWindow : EditorWindow
+{
+    [MenuItem("Tools/CustomWindow")]
+    public static void ShowWindow()
+    {
+        GetWindow<CustomEditorWindow>("CustomEditorWindow");
+    }
+    void OnGUI()
+    {
+        GUILayout.Label("Reload Item Database", EditorStyles.boldLabel);
+        if (GUILayout.Button("Reload Items"))
+        {
+            GameObject.Find("Databases").GetComponent<LoadExcel>().LoadItemData();
+        }
+    }
+}
