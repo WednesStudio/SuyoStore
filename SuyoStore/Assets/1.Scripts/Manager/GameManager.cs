d1406c61f7cb020bc30c412626a85f7eca48a73d
Suya <77002255+suyaneeko@users.noreply.github.com>
2022-04-24 14:27:51 +0900

    Item Image Update & Bug fix

--- SuyoStore/Assets/1.Scripts/Manager/GameManager.cs
+++ SuyoStore/Assets/1.Scripts/Manager/GameManager.cs
@@ -139,8 +139,8 @@ public class GameManager : MonoBehaviour
     {
         Item temp = _dataManager.SetNewItem(itemID);
         string category = _dataManager.GetItemSubCategory(itemID);
-
         if (category != "가방" && category != "스마트폰") _dataManager.AddItem(itemID, -1);
+        
         _itemUse.UseItem(itemID);
     }
 
