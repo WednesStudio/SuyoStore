cf9d6b6628e62e634c2b30fe9a2e8ca5badbf312
Ise이제 <ydkrkek@gmail.com>
2022-04-29 16:04:20 +0900

    Update PlayerSpawner.cs
    수정 중

--- SuyoStore/Assets/1.Scripts/Player/PlayerSpawner.cs
+++ SuyoStore/Assets/1.Scripts/Player/PlayerSpawner.cs
@@ -30,79 +30,63 @@ public class PlayerSpawner : MonoBehaviour
         {
             Debug.Log("Gate On");
             GameManager.GM.ChangeToOtherScene(-1);
-            if (this.dirType == eDirection.UP || this.dirType == eDirection.DOWN)
-            {
-                player.GetComponent<PlayerController>().isChangeFloor = true;
-                player.transform.position = ArrivePoint.transform.position;
-            }
-            //if (this.dirType == eDirection.UP)
-            //{
-            //    Debug.Log("Up");
-            //    GoUp();
-            //}
-            //else if (this.dirType == eDirection.DOWN)
-            //{
-            //    Debug.Log("Down");
-            //    GoDown();
-            //}
-            //else { }
-            //timer += Time.deltaTime;
-            //if(timer >= stayEscalatorTime)
-            //{
+            player.GetComponent<Transform>().position += Vector3()
 
-            //GameManager.gm. ==true position ¹Ù²Ù±â
-            //timer = 0.0f;
+            //if (this.dirType == eDirection.UP || this.dirType == eDirection.DOWN)
+            //{
+            //    player.GetComponent<PlayerController>().isChangeFloor = true;
+            //    player.transform.position = ArrivePoint.transform.position;
             //}
         }
     }
 
-    void GoUp()
-    {
-        switch (targetFloor)
-        {
-            case eTargetFloor.B1:
-                ArrivePoint = GameObject.Find("Up-arrive B1");
-                break;
-            case eTargetFloor.F1:
-                ArrivePoint = GameObject.Find("Up-arrive F1");
-                break;
-            case eTargetFloor.F2:
-                ArrivePoint = GameObject.Find("Up-arrive F2");
-                break;
-            case eTargetFloor.F3:
-                ArrivePoint = GameObject.Find("Up-arrive F3");
-                break;
-            case eTargetFloor.Looftop:
-                Debug.Log("This floor need MUST ITEM.");
-                break;
-            default:
-                Debug.Log("This floor is last. Can't go Up.");
-                break;
-        }
-        player.transform.position = ArrivePoint.transform.position;
-    }
+    //void GoUp()
+    //{
+    //    switch (targetFloor)
+    //    {
+    //        case eTargetFloor.B1:
+    //            ArrivePoint = GameObject.Find("Up-arrive B1");
+    //            break;
+    //        case eTargetFloor.F1:
+    //            ArrivePoint = GameObject.Find("Up-arrive F1");
+    //            break;
+    //        case eTargetFloor.F2:
+    //            ArrivePoint = GameObject.Find("Up-arrive F2");
+    //            break;
+    //        case eTargetFloor.F3:
+    //            ArrivePoint = GameObject.Find("Up-arrive F3");
+    //            break;
+    //        case eTargetFloor.Looftop:
+    //            Debug.Log("This floor need MUST ITEM.");
+    //            break;
+    //        default:
+    //            Debug.Log("This floor is last. Can't go Up.");
+    //            break;
+    //    }
+    //    player.transform.position = ArrivePoint.transform.position;
+    //}
 
-    void GoDown()
-    {
-        switch (targetFloor)
-        {
-            case eTargetFloor.B2:
-                ArrivePoint = GameObject.Find("Down-arrive B2");
-                break;
-            case eTargetFloor.B1:
-                ArrivePoint = GameObject.Find("Down-arrive B1");
-                break;
-            case eTargetFloor.F1:
-                ArrivePoint = GameObject.Find("Down-arrive F1");
-                break;
-            case eTargetFloor.F2:
-                ArrivePoint = GameObject.Find("Down-arrive F2");
-                break;
-            default:
-                Debug.Log("This floor is last. Can't go Down.");
-                break;
-        }
-        player.transform.position = ArrivePoint.transform.position;
-    }
+    //void GoDown()
+    //{
+    //    switch (targetFloor)
+    //    {
+    //        case eTargetFloor.B2:
+    //            ArrivePoint = GameObject.Find("Down-arrive B2");
+    //            break;
+    //        case eTargetFloor.B1:
+    //            ArrivePoint = GameObject.Find("Down-arrive B1");
+    //            break;
+    //        case eTargetFloor.F1:
+    //            ArrivePoint = GameObject.Find("Down-arrive F1");
+    //            break;
+    //        case eTargetFloor.F2:
+    //            ArrivePoint = GameObject.Find("Down-arrive F2");
+    //            break;
+    //        default:
+    //            Debug.Log("This floor is last. Can't go Down.");
+    //            break;
+    //    }
+    //    player.transform.position = ArrivePoint.transform.position;
+    //}
 
 }
