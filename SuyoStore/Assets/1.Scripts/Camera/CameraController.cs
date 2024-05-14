be6245d350d6a61a36ace18c3681b7760c795cf7
YeEun <ydkrkek@gmail.com>
2024-05-14 17:19:13 +0900

    [Fix] 카메라랑 Player 사이에 Floor Layer 가진 오브젝트가 가리는 거 수정 Layer가 Wall일 때랑 Floor일 때랑 수치 다르게 수정

--- SuyoStore/Assets/1.Scripts/Camera/CameraController.cs
+++ SuyoStore/Assets/1.Scripts/Camera/CameraController.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 	[SerializeField] Define.CameraMode camMode = Define.CameraMode.QuaterView;
-	[SerializeField] Vector3 delta = new Vector3(0f, 4f, -4f);
+	[SerializeField] Vector3 delta = new Vector3(0f, 6f, -4.6f);
+	[SerializeField] Vector3 camUp = new Vector3(0f, 1.6f, 0f);
 	[SerializeField] GameObject target = null;
 
 	private void Start()
@@ -17,22 +18,26 @@ public class CameraController : MonoBehaviour
 
 	void LateUpdate()
     {
-        if (camMode == Define.CameraMode.QuaterView)
+		if (camMode == Define.CameraMode.QuaterView)
         {
-			// 장애물 무시
-			LayerMask mask = LayerMask.GetMask("Floor") | LayerMask.GetMask("Wall");
+			// 장애물이 타겟과 카메라 사이에 있을 때: 타겟과 카메라 사이의 간격 좁히기
 			RaycastHit hit;
-			if(Physics.Raycast(target.transform.position, delta, out hit, delta.magnitude, mask))
+			if (Physics.Raycast(target.transform.position, delta, out hit, delta.magnitude, LayerMask.GetMask("Wall")))
 			{
-				Vector3 camUp = new Vector3(0f, 2f, 0f);
 				float dist = (hit.point - target.transform.position).magnitude * 0.8f;
+				transform.position = target.transform.position + camUp + delta.normalized * dist;
+			}
+			else if (Physics.Raycast(target.transform.position, delta, out hit, delta.magnitude, LayerMask.GetMask("Floor")))
+			{
+				Debug.Log("땅!!!");
+				float dist = (hit.point - target.transform.position).magnitude * 0.5f;
 				transform.position = target.transform.position + camUp + delta.normalized * dist;
 			}
 			else { 
 				// 카메라 positoin 수정
 				transform.position = target.transform.position + delta;
 			}
-			transform.LookAt(target.transform);
+			transform.LookAt(target.transform.position + camUp);
 		}
 	}
 
